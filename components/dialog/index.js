3555f7b72d3a54ce24505a3d9f3c2498c60a94db
Tushar Tiwari <tushartiwari0172@gmail.com>
2022-01-26 18:34:57 +0530

    add: layout-2 for dialog component

--- components/dialog/index.js
+++ components/dialog/index.js
@@ -1,10 +1,20 @@
-const dialog = document.querySelector('#dialog');
+const dialog1 = document.querySelector('#dialog1');
+const dialog2 = document.querySelector('#dialog2');
 
-const showDialog = () => {
-  dialog.style.visibility = 'visible';
-  dialog.style.opacity = 1;
+const showDialog1 = () => {
+  dialog1.style.visibility = 'visible';
+  dialog1.style.opacity = 1;
 }
-const hideDialog = () => {
-  dialog.style.visibility = 'hidden';
-  dialog.style.opacity = 0;
+const hideDialog1 = () => {
+  dialog1.style.visibility = 'hidden';
+  dialog1.style.opacity = 0;
+}
+
+const showDialog2 = () => {
+  dialog2.style.visibility = 'visible';
+  dialog2.style.opacity = 1;
+}
+const hideDialog2 = () => {
+  dialog2.style.visibility = 'hidden';
+  dialog2.style.opacity = 0;
 }
