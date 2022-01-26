2bb4cd691820d15ca461504bf98b4bf02d7a304b
Tushar Tiwari <tushartiwari0172@gmail.com>
2022-01-26 16:46:16 +0530

    add: components/dialog

---
+++ components/dialog/index.js
@@ -0,0 +1,10 @@
+const dialog = document.querySelector('#dialog');
+
+const showDialog = () => {
+  dialog.style.visibility = 'visible';
+  dialog.style.opacity = 1;
+}
+const hideDialog = () => {
+  dialog.style.visibility = 'hidden';
+  dialog.style.opacity = 0;
+}
